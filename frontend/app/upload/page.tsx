f1cc63d547252da7055ed8d9a83ae204b81b0498
Abinesh s <119126115+Abinesh-ss@users.noreply.github.com>
2026-03-28 01:07:47 +0530

    Update page.tsx

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from "react";
 import Layout from "@/components/Layout";
 import { supabase } from "@/lib/supabase"; 
